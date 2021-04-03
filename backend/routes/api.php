7a70a0b88d0c95c39d94fe15128ae58771750d1e
Zakir Hossen <web.zakirbd@gmail.com>
2021-04-03 23:10:34 +0600

    laravel jwt package install

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+
+    'middleware' => 'api',
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
